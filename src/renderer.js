637c86039a5815ba3b42b5e306323e89e886529e
MoyuScript <45550579+MoyuScript@users.noreply.github.com>
2024-01-12 14:45:54 +0800

    fix

--- src/renderer.js
+++ src/renderer.js
@@ -2,10 +2,10 @@
   // MacOS 上红绿灯默认在标题栏左侧，因此需要将窗口标题文字让开以避免遮挡，这里设为居中。
   // 需要注意这里使用了 electron-vite 框架，如果你没有使用该框架，可在 Node 后端将 process.platform 传递给前端进行判断。
   // 'darwin' 代表 MacOS
-  if (window.electron.platform === 'darwin') {
+  if (window.electron.process.platform === 'darwin') {
     document.querySelector('.custom-titlebar').classList.add('darwin')
   }
-  if (window.electron.platform === 'linux') {
+  if (window.electron.process.platform === 'linux') {
     document.querySelector('.custom-titlebar').classList.add('linux')
   }
 })()
